968b60c8edf884e939d244dc17bc2e64ba48d05c
James Laverack <james@jameslaverack.com>
2016-03-29 19:50:45 +0100

    Add argument for ASCII mode
    ASCII mode outputs in only ASCII characters, and is enabled using the `--ascii` flag.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,24 @@ use std::io::{self, Write};
 use std::collections::HashMap;
 use rand::{ThreadRng, Rng};
 use std::time::Duration;
+use std::env;
 
 struct Grid {
     cells: HashMap<(i32, i32), bool>,
     width: i32,
     height: i32,
     rng: ThreadRng,
+    ascii: bool
 }
 
 impl Grid {
-    pub fn new(width: i32, height: i32) -> Self {
+    pub fn new(width: i32, height: i32, ascii: bool) -> Self {
         let mut grid = Grid {
             cells: HashMap::new(),
             width: width,
             height: height,
             rng: rand::thread_rng(),
+            ascii: ascii
         };
 
         grid.each_position(|grid, coord| {
@@ -103,11 +106,26 @@ impl Grid {
         return Ok(());
     }
 
+    fn print_alive_cell(&self, output: &mut Write) -> io::Result<usize> {
+        if self.ascii {
+            return output.write(b"x");
+        } else {
+            return output.write("\u{2588}".as_bytes());
+        }
+    }
+
+    fn print_dead_cell(&self, output: &mut Write) -> io::Result<usize> {
+        if self.ascii {
+            return output.write(b".");
+        } else {
+            return output.write("\u{2591}".as_bytes());
+        }
+    }
 
     fn print_cell(&self, state: bool, output: &mut Write) -> io::Result<()> {
         let result = match state {
-            true => output.write("\u{2588}".as_bytes()),
-            false => output.write("\u{2591}".as_bytes()),
+            true => self.print_alive_cell(output),
+            false => self.print_dead_cell(output)
         };
 
         try!(result);
@@ -118,7 +136,8 @@ impl Grid {
 }
 
 fn main() {
-    let mut g = Grid::new(70, 30);
+    let ascii: bool = env::args().find(|s| s == "--ascii") != None;
+    let mut g = Grid::new(70, 30, ascii);
     let mut stdout = io::stdout();
     g.randomize();
     let sleep_time = Duration::from_millis(100);
